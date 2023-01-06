a6f8c3eb162dd96665178b740ad58ff5107bc5a5
Gautam Adatra <gautamkiran123@gmail.com>
2023-01-07 02:11:22 +0530

    chore: made changes to backend to solve error

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import {
 import {
   loginHandler,
   signupHandler,
-  userProfilehandler,
 } from "./backend/controllers/AuthController";
 import {
   archiveNoteHandler,
@@ -56,7 +55,7 @@ export function makeServer({ environment = "development" } = {}) {
       this.post("/auth/login", loginHandler.bind(this));
 
       // user route (private)
-      this.get("/user", userProfilehandler.bind(this));
+      // this.get("/user", userProfilehandler.bind(this));
 
       // notes routes (private)
       this.get("/notes", getAllNotesHandler.bind(this));
